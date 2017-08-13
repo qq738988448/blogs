53824ec09cca7514c56ee1347a8d4e647f158646
wanghan <wanghan@ziyitong.com.cn>
2017-08-13 23:01:16 +0800

    提交阿里云镜像

--- blogs-web/src/test/java/com/example/blogs/BlogsApplicationTests.java
+++ blogs-web/src/test/java/com/example/blogs/BlogsApplicationTests.java
@@ -2,15 +2,22 @@ package com.example.blogs;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class BlogsApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
