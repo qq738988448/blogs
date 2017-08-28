ce4491c89c8940ad3f1843ab26d9cb810b2db720
wanghan <wanghan@ziyitong.com.cn>
2017-08-28 11:31:30 +0800

    提交整合mybaits

--- blogs-web/src/main/java/com/example/blogs/BlogsApplication.java
+++ blogs-web/src/main/java/com/example/blogs/BlogsApplication.java
@@ -1,8 +1,9 @@
 package com.example.blogs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.example.blogs.dao")
 @SpringBootApplication
 public class BlogsApplication {
 
--- blogs-web/src/test/java/com/example/blogs/BlogsApplicationTests.java
+++ blogs-web/src/test/java/com/example/blogs/BlogsApplicationTests.java
@@ -1,23 +1,41 @@
 package com.example.blogs;
 
+import com.example.blogs.dao.UsersMapper;
+import com.example.blogs.model.Users;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@MapperScan("com.example.blogs.dao")
+@EnableTransactionManagement
 public class BlogsApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UsersMapper usersMapper;
+
     @Test
     public void contextLoads() {
-
+        Users users = usersMapper.selectByPrimaryKey(1L);
+        users.setPassword("123456");
+        usersMapper.updateByPrimaryKey(users);
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println();
     }
 
 }
